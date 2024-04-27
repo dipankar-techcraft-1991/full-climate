da3378035ea83f008e4b1051a71e1df3bd3168f0
Dipankar Majumdar <dipmajumdar1991@gmail.com>
2024-04-27 11:42:27 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import search from "./assets/icons/search.svg";
 import BackgroundLayout from "./components/BackgroundLayout";
 import WeatherCard from "./components/WeatherCard";
